6688a2eb03b52cebf0b4b86ec284150c2f574f1d
Viktor Stankevich <stankevichviktor@inbox.ru>
2023-09-19 11:44:09 +0700

    Lesson 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribe } from 'diagnostics_channel';
 
 // LESSON #1
 
@@ -55,5 +56,72 @@ import './App.css';
 
 
 // const App = () => <Title title="test" />
+
+//________________________________________________________________________________________
+
+// LESSON #3
+// Типизация хуков
+const App: React.FC = () => {
+
+  // -----------useState------------
+  // Если ипользуется простое значение то типизация не трубуется
+  // const [value, setValue] = useState(0);
+
+  // Если значение комплексоное или может отсутствовать то типизация нужна
+
+  // const [value, setValue] = useState<number | undefined>(undefined);
+  // const [value, setValue] = useState<Array<number>>([]);
+
+  // Или использовать "type" или "inteface"
+
+  // interface IUser {
+  //   name: string;
+  //   age?: number
+  // }
+
+  // const [value, setValue] = useState<IUser>({ name: 'Viktor' })
+
+  // ---------- useRef -----------
+  // const ref1 = useRef<HTMLElement>(null!);
+  // const ref2 = useRef<HTMLElement | null>(null)
+
+  // ---------- useContext -----------
+  // Это чистая функция и не требует типизации
+
+  // ---------- useReducer ----------
+  // interface State { count: number; }
+
+  // type Action = { type: 'increment' | 'decrement' }
+
+  // const counterReducer = ({ count }: State, { type }: Action) => {
+  //   switch (type) {
+  //     case 'increment': return { count: count + 1 };
+  //     case 'decrement': return { count: count - 1 };
+  //     default: return {};
+  //   }
+  // };
+
+  // const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
+
+  // dispatch({ type: 'increment' })
+  // dispatch({ type: 'decrement' })
+
+  // ---------- useCallback & useMemo ----------
+  // Чистые функции. Явная типизация не трубуется
+
+  // ---------- useEffect & useLayoutEffect ----------
+  // Явная типизация не требуется
+
+  // useEffect(() => {
+  //   const subscriber = subscribe(options);
+  //   return () => {
+  //     unsubscribe();
+  //   }
+  // }, [options])
+
+  return null
+
+}
 
 export default App;
